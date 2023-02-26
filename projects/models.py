a0193deb0e5d37d0479e8867d45d2e8f983d9673
joe-sacco <102517425+joe-sacco@users.noreply.github.com>
2023-02-26 21:51:15 +0900

    feat: Add first db model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200) #required field
+    description = models.TextField(null=True, blank=True) #not required
+    demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True) #auto generate timestamp
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False) # must be unique
