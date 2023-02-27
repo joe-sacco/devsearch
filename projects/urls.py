8a0790640da0c40b9ff23a031bcd66fac9d5952f
joe-sacco <102517425+joe-sacco@users.noreply.github.com>
2023-02-27 11:41:04 +0900

    feat: Add create form button and view

--- projects/urls.py
+++ projects/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.projects, name='projects'),
     # path can change as long as name stays the say it wont disconnect in other files
-    path('project/<str:pk>', views.project, name='project') 
+    path('project/<str:pk>', views.project, name='project'),
+    path('create-project/', views.createProject, name='create-project')
 ]
--- projects/views.py
+++ projects/views.py
@@ -2,23 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Project
 
-projectsList = [
-    {
-    'id': '1', 
-    'title': "Ecommerce website", 
-    'description': 'Fully funciontal ecommerce website'
-    },
-    {
-    'id': '2',
-    'title': 'Porfolio Website',
-    'description': 'This was a projt where I built out my portfolio.'
-    },
-    {
-    'id': '3', 
-    'title': 'Social Network',
-    'description': 'Awesome open source project I am still working on.'
-    }
-]
 # Create your views here.
 
 #HTTP request -> response
@@ -27,7 +10,10 @@ def projects(request):
     context = {'projects': projects}
     return render(request, 'projects/projects.html', context)
    
-
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
     return render(request, 'projects/single-project.html', {'project': projectObj})
+
+def createProject(request):
+    context = {}
+    return render(request, "projects/project_form.html", context)
