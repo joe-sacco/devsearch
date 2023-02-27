664f7b05cc65ef3e27601866882065131bcba48f
joe-sacco <102517425+joe-sacco@users.noreply.github.com>
2023-02-27 18:58:15 +0900

    feat: Add updateProject

--- projects/urls.py
+++ projects/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.projects, name='projects'),
     # path can change as long as name stays the say it wont disconnect in other files
     path('project/<str:pk>', views.project, name='project'),
-    path('create-project/', views.createProject, name='create-project')
+    path('create-project/', views.createProject, name='create-project'),
+    path('update-project/<str:pk>/', views.updateProject, name='update-project'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -29,3 +29,17 @@ def createProject(request):
     context = {'form': form}
     return render(request, "projects/project_form.html", context)
 
+def updateProject(request, pk):
+    project = Project.objects.get(id=pk)
+    #django will prefill form with project data
+    form = ProjectForm(instance=project)
+
+    if request.method == 'POST':
+        # let django know what project to update w/ instance
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {'form': form}
+    return render(request, "projects/project_form.html", context)
